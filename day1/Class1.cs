e9461af663b961bcf089040d20aa326c188f88e1
cho7rose <cho7rose@hotmail.com>
2021-12-19 18:57:55 +0000

    Star2

--- day1/Class1.cs
+++ day1/Class1.cs
@@ -18,4 +18,17 @@ public class Day1
         Console.WriteLine("There are {0} increases", increases);
         return increases;
     }
+    public static int GetStar2(string raw)
+    {
+        double[] input = Array.ConvertAll(raw.Split(new[] { Environment.NewLine }, StringSplitOptions.None), s => double.Parse(s));
+        int increases = 0;
+        for(int i=2; i<input.Length-1; i++)
+        {
+            double a = input[i-2]+input[i-1]+input[i];
+            double b = input[i-1]+input[i]+input[i+1];
+            increases += a<b ? 1 : 0;
+        }
+        Console.WriteLine("There are {0} sums larger than the previous sum", increases);
+        return increases;
+    }
 }
