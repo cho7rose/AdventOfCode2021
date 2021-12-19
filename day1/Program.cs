c40b6cae6d50381a31379334e2f1d4fe797769eb
cho7rose <cho7rose@hotmail.com>
2021-12-19 17:19:28 +0000

    Day1 - Star1

---
+++ day1/Program.cs
@@ -0,0 +1,10 @@
+using System;
+using AdventOfCode;
+
+class Program
+    {
+        static void Main (string[] args)
+        {
+            AdventOfCode.Day1.GetStar1(System.IO.File.ReadAllText("input.csv"));
+        }
+    }
---
+++ day1/tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using AdventOfCode;
+using FluentAssertions;
+
+namespace tests;
+
+public class UnitTest1
+{
+    public static readonly string Sample = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+    [Fact]
+    public void Test1()
+    {
+        var expected = 7;
+        var actual = Day1.GetStar1(Sample);
+        Assert.Equal(expected, actual);
+    }
+}
