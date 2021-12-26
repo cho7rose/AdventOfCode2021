4eb1ebad5b8ad335963587900d76ec4ca1b90a66
cho7rose <62804911+cho7rose@users.noreply.github.com>
2021-12-26 11:38:15 +0000

    Day3 (#4)
    * day2
    
    * Added day3

---
+++ day3/Program.cs
@@ -0,0 +1,12 @@
+using System;
+using AdventOfCode;
+
+class Program
+    {
+        static void Main (string[] args)
+        {
+            AdventOfCode.day3.GetStar1(System.IO.File.ReadAllText("input.csv"));
+            AdventOfCode.day3.GetStar2(System.IO.File.ReadAllText("input.csv"));
+
+        }
+    }
--- day3/tests-day3/UnitTest1.cs
+++ day3/tests-day3/UnitTest1.cs
@@ -1,12 +1,34 @@
 using Xunit;
+using AdventOfCode;
 
 namespace tests_day3;
 
 public class UnitTest1
 {
+    public static readonly string Sample = @"00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010";
     [Fact]
     public void Test1()
     {
-
+        var expected = 198;
+        var actual = day3.GetStar1(Sample);
+        Assert.Equal(actual, expected);
+    }
+    [Fact]
+    public void Test2()
+    {
+        var expected = 230;
+        var actual = day3.GetStar2(Sample);
+        Assert.Equal(actual, expected);
     }
 }
